f3e5cb97a313aac5a9ea72c9c1a85d227f4738cc
Jennifer Harkreader <jen3072@gmail.com>
2017-09-28 16:18:09 -0700

    Initial commit

---
+++ src/main/java/org/launchcode/matchfour/MatchFourApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.matchfour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchFourApplication.class, args);
+	}
+}
