ff84365840402dd98a72f4ad1d58fe40dee9ba91
Jennifer Harkreader <jen3072@gmail.com>
2017-10-01 16:29:05 -0700

    CodePeg class

---
+++ src/main/java/org/launchcode/matchfour/models/CodePeg.java
@@ -0,0 +1,29 @@
+package org.launchcode.matchfour.models;
+
+
+import java.util.Random;
+
+public class CodePeg {
+
+    // Using a number instead of color, connection to color or shape will happen on front end
+    private int pegValue;
+
+    CodePeg() {
+        Random ran = new Random();
+        int newNumber = ran.nextInt(6);
+        this.setPegValue(newNumber);
+    }
+
+    CodePeg(int num) {
+        this();
+        this.setPegValue(num);
+    }
+
+    public int getPegValue() {
+        return pegValue;
+    }
+
+    public void setPegValue(int pegValue) {
+        this.pegValue = pegValue;
+    }
+}
