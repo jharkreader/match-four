5b3b294042bdcd2efb6bbef35e8c27adc7adbd20
Diane Zevenbergen <dianezev@comcast.net>
2017-10-10 18:34:33 -0700

    added vars& fcn for users times

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -12,13 +12,19 @@
 //    (guessCtr would be initialized to 1 & then a fcn
 //    would add 'currentTry' className to the relevant guessing row (& remove from others)
 
+// Global variables - so that Java, etc can access any of these 3 vars...
+var myBestTime = -1;  // keep track of user's best time for current session in seconds
+var timeSeconds = 0;  // user's total seconds for most recent game
+var timeString = "";  // string that shows mins:seconds for most recent game
+
 (function() {
   
-    guesses = 8;
-    guessCtr = 1;
-    colorPick = 1;
-    secret = [0, 0, 0, 0];
-    curGuess = [0, 0, 0, 0];
+    var guesses = 8;
+    var guessCtr = 1;
+    var colorPick = 1;
+    var secret = [0, 0, 0, 0];
+    var curGuess = [0, 0, 0, 0];
+    var startTime;
 
   /******************************************************************
    * EVENT LISTENERS
@@ -67,14 +73,24 @@
     
     // Otherwise:
     
+    // If guessCtr = 1, record start time (millisecs)
+    if (guessCtr === 1) {
+      startTime = new Date().getTime();
+    }
+
     // call method 'compareSlots' to compare elements in 'secret' to 'curGuess' - return object or array with
     // #black and # white pegs
     
     // call method 'setBlackWhitePegs' (pass in #black & #white peg counts) to set background colors for 
     // the 'peg' class within the 'currentTry' section
     
-    // if #black = 4 call method userMsg('You got it!...') to display a message or route to another pg?
-    // (later - if best score, save to user's info in db.)
+    // if #black = 4 call method , call fcn to set time variables, give user message, etc. 
+    //if (# black pegs === 4) {
+      // checkTimeVsBest(); // this function sets global variables related to time (myBestTime, timeSeconds, timeString)
+      //
+      // then some user message... to display a message or route to another pg?
+      // (later - if best score, save to user's info in db.)
+    //}
     
     // else if guessCtr = 8  call method userMsg('sorry...blah blah') to display a message or route to another pg?
     // (later on it would be good to show the actual solution to user).
@@ -127,6 +143,8 @@
     // Set up new gameboard:
     guessCtr = 1;
     curGuess = [0, 0, 0, 0];
+    startTime = 0;
+    endTime = 0;
     setSecret(); 
     setCurrentTry();
     setCurrentPicker(colorPick);  // really just needed on init
@@ -215,6 +233,32 @@
   
   /***** these are related to the game logic, calcs, comparisons, etc.****/
   
+// Get time for most recent game, compare to user's best time
+// Sets 3 GLOBAL variables for java to use as needed (timeSeconds, myBestTime and timeString)
+// TBD: this could set (or return) a more complete message if that's helpful - like
+// "Nice job! You finished in 3 minutes, 23 seconds." 
+// and/or "Congrats! You got your best time today: 3 minutes, 23 seconds."
+function checkTimeVsBest() {
+  var endTime = new Date().getTime();
+  var mins;
+  var seconds:
+
+  timeSeconds = endTime - startTime;
+  mins = Math.floor(timeSeconds/60);
+  seconds = timeSeconds % 60;
+
+  // Compare time to best time & reset if user improved (best is initialized to -1)
+  if ((myBestTime === -1) || (timeSeconds < myBestTime)) {
+    myBestTime = timeSeconds;
+  }
+
+  // Create readable string that shows minutes & seconds - for use in a user msg
+  timeString = mins + 
+              (mins === 1 ? " minute, " : " minutes, ") + 
+              seconds + 
+              (seconds === 1 ? " second." : " seconds.");
+}
+
   // Compare secret to curGuess and return obj or array with #black & # white
   function compareSlots() {
     // TBD
