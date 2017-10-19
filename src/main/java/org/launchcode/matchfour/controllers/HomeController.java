511f5dced025a2fae25a999602b42821b053eca2
mikupgirl <mikupgirl@gmail.com>
2017-10-18 19:47:39 -0700

    moved logincontroller code to homeController, deleted loginController java class

--- src/main/java/org/launchcode/matchfour/controllers/HomeController.java
+++ src/main/java/org/launchcode/matchfour/controllers/HomeController.java
@@ -4,6 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.launchcode.matchfour.models.User;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("")
 
@@ -29,5 +36,26 @@ public class HomeController {
 
         model.addAttribute("title", "Game Over!" );
         return "gameover";
+    }
+
+
+    @RequestMapping(value = "userLogin", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title", "Welcome to MatchFour!");
+        model.addAttribute("user", new User());
+
+        return "userLogin";
+    }
+
+    @RequestMapping(value = "userLogin", method = RequestMethod.POST)
+    public String add(@ModelAttribute @Valid User user, Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "userLogin";
+        }
+
+        model.addAttribute("title", "user");
+        model.addAttribute("user", new User());
+        return "redirect::/play"; //main html page for the game
     }
 }
