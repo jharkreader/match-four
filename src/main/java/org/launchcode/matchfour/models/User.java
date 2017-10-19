15ac3d52e8f19d3ab51656c7ec8a4b2707e7ffad
mikupgirl <mikupgirl@gmail.com>
2017-10-18 21:00:16 -0700

    created password form in html page and user object in user class

--- src/main/java/org/launchcode/matchfour/models/User.java
+++ src/main/java/org/launchcode/matchfour/models/User.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+//TODO: add param for password and user signup params
 
 public class User {
 
@@ -11,8 +12,13 @@ public class User {
     @Size(min=3, max=15)
     private String name;
 
+    @NotNull
+    @Size(min=3, max=15)
+    private String password;
+
     public User(String name) {
         this.name = name;
+        this.password = password;
     }
 
     public User() {}
@@ -23,6 +29,14 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
 
