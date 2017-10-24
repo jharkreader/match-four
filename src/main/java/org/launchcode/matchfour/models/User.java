a115b0de007cf464c4ceab655270588e67f28bf1
Jennifer Harkreader <jen3072@gmail.com>
2017-10-23 20:25:38 -0700

    Added @Entity annotation to User, generated id field

--- src/main/java/org/launchcode/matchfour/models/User.java
+++ src/main/java/org/launchcode/matchfour/models/User.java
@@ -4,8 +4,12 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
 
     @NotNull
     @Size(min=3, max=15)
@@ -23,6 +27,10 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
     }
 }
 
